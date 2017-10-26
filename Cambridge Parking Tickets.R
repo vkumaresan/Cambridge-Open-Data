726a600ff675b97d4a56edb579506a2d00e6ea19
vkumaresan <33133050+vkumaresan@users.noreply.github.com>
2017-10-26 14:13:09 -0400

    Add files via upload
    Code for first Parking Tickets blog post.

---
+++ Cambridge Parking Tickets.R
@@ -0,0 +1,51 @@
+# Cambridge Open Data- Cambridge Parking Tickets
+### Viggy Kumaresan###
+
+#Import CSV File From Open Data Portal
+
+#Import packages
+library(stringr)
+library(ggmap)
+library(ggplot2)
+
+#Extract and split lat/long values using gsub and str_split
+
+# use gsub to create extracted long_lat variable
+# use str_split to split the longitude and latitude
+
+split_coords <- str_split(string = Cambridge_Parking_Tickets$long_lat, pattern = ',', n = 2, simplify = TRUE)
+
+split_coords <- as.data.frame(split_coords)
+split_coords$lat <- as.numeric(as.character(split_coords$V1))
+split_coords$long <- as.numeric(as.character(split_coords$V2))
+
+str(split_coords)
+
+Cambridge_Parking_Tickets$long <- split_coords$lat
+Cambridge_Parking_Tickets$lat <- split_coords$long
+
+split_coords <- as.data.frame(split_coords)
+split_coords$lat <- as.numeric(as.character(split_coords$V1))
+split_coords$long <- as.numeric(as.character(split_coords$V2))
+
+# Get Cambridge map(optional, for later R visualization. Use Tableau for now)
+cambridge.limits <- geocode(c("Cambridge, USA"))
+cambridge.limits
+
+mapcambridge <- get_map(location = c(lon = mean(split_coords$long), lat = mean(split_coords$long)), zoom = 225,
+                      maptype = "satellite", scale = 10)
+
+ggmap(mapcambridge) +
+  geom_point(data = split_coords, aes(x = long, y = lat, fill = "red", alpha = 0.8), size = 5, shape = 21) +
+  guides(fill=FALSE, alpha=FALSE, size=FALSE)
+
+# Exploratory Analysis
+table(Cambridge_Parking_Tickets$`Violation Description`)
+
+
+# Write CSV files for Tableau Visualization
+write.csv(split_coords, file='/Users/Viggy/Desktop/Data Science/R/Cambridge Open Data/Cambridge_Parking_Tickets_splitcoords.csv')
+
+write.csv(Cambridge_Parking_Tickets, file='/Users/Viggy/Desktop/Data Science/R/Cambridge Open Data/Cambridge_Parking_Tickets_longlat.csv')
+
+
